25edc8d1e9798242e36fb29fe3dd268b311e74fb
rosy14514 <rosy.ram@mastek.com>
2021-05-19 16:48:04 +0530

    Initial commit

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUpload.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private static IWebHostEnvironment _webHostEnvironment;
+        public FileUploadController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public async Task<string> Upload([FromForm] UploadFile obj) 
+        {
+            if (obj.Files.Length > 0)
+            {
+                try
+                {
+                    if (!Directory.Exists(_webHostEnvironment.WebRootPath +"\\Images\\"))
+                    {
+                        Directory.CreateDirectory(_webHostEnvironment.WebRootPath + "\\Images\\");
+                    }
+                    using(FileStream filestream = System.IO.File.Create(_webHostEnvironment.WebRootPath + "\\Images\\"+obj.Files.FileName))
+                    {
+                        obj.Files.CopyTo(filestream);
+                        filestream.Flush();
+                        return "\\Images\\" + obj.Files.FileName;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+            else
+            {
+                return "Upload Failed";
+            }
+        }
+
+        [HttpGet("{fileName}")]
+        public async Task<IActionResult> Get([FromRoute] string fileName)
+        {
+            string path = _webHostEnvironment.WebRootPath + "\\Images\\";
+            var filepath = path + fileName + ".jpg";
+            if(System.IO.File.Exists(filepath))
+            {
+                byte[] b = System.IO.File.ReadAllBytes(filepath);
+                return File(b, "image/jpg");
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/UploadFile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.Models
+{
+    public class UploadFile
+    {
+        public int Id { get; set; }
+
+        public IFormFile Files { get; set; }
+
+        public string Name { get; set; }
+    }
+}
